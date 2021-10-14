6d030fb78cafd05a9bdb06055f9642369dde4490
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-10-14 22:21:59 +0200

    Fix time parsing

--- src/main/java/fr/raksrinana/twitchminer/api/ws/data/message/channellastviewedcontentupdated/ChannelLastViewedContentUpdatedData.java
+++ src/main/java/fr/raksrinana/twitchminer/api/ws/data/message/channellastviewedcontentupdated/ChannelLastViewedContentUpdatedData.java
@@ -2,12 +2,12 @@ package fr.raksrinana.twitchminer.api.ws.data.message.channellastviewedcontentup
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import fr.raksrinana.twitchminer.utils.json.TwitchTimestampDeserializer;
+import fr.raksrinana.twitchminer.utils.json.ISO8601ZonedDateTimeDeserializer;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import java.time.Instant;
+import java.time.ZonedDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -15,8 +15,8 @@ import java.time.Instant;
 @ToString
 public class ChannelLastViewedContentUpdatedData{
 	@JsonProperty("timestamp")
-	@JsonDeserialize(using = TwitchTimestampDeserializer.class)
-	private Instant timestamp;
+	@JsonDeserialize(using = ISO8601ZonedDateTimeDeserializer.class)
+	private ZonedDateTime timestamp;
 	@JsonProperty("channel_last_viewed_content")
 	private ChannelLastViewedContent channelLastViewedContent;
 }
