3ed72b493c0ca5f81208743c25d2c422d232b54f
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-10-31 14:57:37 +0100

    Update doc

--- src/test/java/fr/raksrinana/twitchminer/factory/TimeFactoryTest.java
+++ src/test/java/fr/raksrinana/twitchminer/factory/TimeFactoryTest.java
@@ -2,11 +2,15 @@ package fr.raksrinana.twitchminer.factory;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class TimeFactoryTest{
 	@Test
 	void create(){
 		assertThat(TimeFactory.now()).isNotNull();
+	}
+	
+	@Test
+	void createZoned(){
+		assertThat(TimeFactory.nowZoned()).isNotNull();
 	}
 }
