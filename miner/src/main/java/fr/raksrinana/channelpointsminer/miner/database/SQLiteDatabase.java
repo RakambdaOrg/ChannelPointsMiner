a22379c591ee0a27e0bcf824a609d9c6c74e4dee
15sawyer <benedikt.westreicher@hotmail.com>
2022-07-09 01:17:02 +0200

    Add sqlite tables.

--- miner/src/main/java/fr/raksrinana/channelpointsminer/miner/database/SQLiteDatabase.java
+++ miner/src/main/java/fr/raksrinana/channelpointsminer/miner/database/SQLiteDatabase.java
@@ -41,7 +41,44 @@ public class SQLiteDatabase extends BaseDatabase{
 				"""
 						CREATE INDEX IF NOT EXISTS `EventDateIdx` ON `Prediction`(`EventDate`);""",
 				"""
-						CREATE INDEX IF NOT EXISTS `EventTypeIdx` ON `Prediction`(`Type`);""");
+						CREATE INDEX IF NOT EXISTS `EventTypeIdx` ON `Prediction`(`Type`);""",
+                """
+						CREATE TABLE IF NOT EXISTS `ResolvedPrediction` (
+							`EventID` VARCHAR(36) NOT NULL PRIMARY KEY,
+							`ChannelID` VARCHAR(32) NOT NULL REFERENCES `Channel`(`ID`),
+							`Title` VARCHAR(64) NOT NULL,
+							`EventCreated` DATETIME NOT NULL,
+							`EventEnded` DATETIME NULL,
+							`Canceled` BOOLEAN NOT NULL,
+							`Outcome` VARCHAR(32) NULL,
+							`Badge` VARCHAR(32) NULL
+						);""",
+                """
+                        CREATE INDEX IF NOT EXISTS `ChannelIDIdx` ON `ResolvedPrediction`(`ChannelID`);""",
+                """
+						CREATE TABLE IF NOT EXISTS `PredictionUser` (
+							`ID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+							`Username` VARCHAR(128) NOT NULL,
+							`PredictionCnt` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+							`WinCnt` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+							UNIQUE (`Username`)
+						);""",
+                """
+                        CREATE INDEX IF NOT EXISTS `UsernameIdx` ON `PredictionUser`(`Username`);""",
+                """
+                        CREATE TABLE IF NOT EXISTS `UserPrediction` (
+                             `ChannelID` VARCHAR(32) NOT NULL REFERENCES `Channel`(`ID`),
+                             `UserID` INT NOT NULL REFERENCES `PredictionUser`(`ID`),
+                             `ResolvedPredictionID` VARCHAR(36) NOT NULL DEFAULT '',
+                             `Badge` VARCHAR(32) NOT NULL,
+                             PRIMARY KEY (`ChannelID`, `UserID`, `ResolvedPredictionID`)
+                        );""",
+                """
+                        CREATE INDEX IF NOT EXISTS `ChannelIDIdx` ON `UserPrediction`(`ChannelID`);""",
+                """
+                        CREATE INDEX IF NOT EXISTS `UserIDIdx` ON `UserPrediction`(`UserID`);""",
+                """
+                        CREATE INDEX IF NOT EXISTS `ResolvedPredictionIDIdx` ON `UserPrediction`(`ResolvedPredictionID`);""");
 	}
 	
 	@Override
