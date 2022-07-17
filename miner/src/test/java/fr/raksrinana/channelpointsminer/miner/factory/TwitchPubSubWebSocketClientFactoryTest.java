81486c5c5851d86f985d3e058914804da4e87a38
Thomas Couchoud <1688389+RakSrinaNa@users.noreply.github.com>
2022-07-17 18:33:39 +0200

    test

--- miner/src/test/java/fr/raksrinana/channelpointsminer/miner/factory/TwitchPubSubWebSocketClientFactoryTest.java
+++ miner/src/test/java/fr/raksrinana/channelpointsminer/miner/factory/TwitchPubSubWebSocketClientFactoryTest.java
@@ -1,13 +1,25 @@
 package fr.raksrinana.channelpointsminer.miner.factory;
 
+import fr.raksrinana.channelpointsminer.miner.api.chat.ws.TwitchChatWebSocketClient;
+import fr.raksrinana.channelpointsminer.miner.api.passport.TwitchLogin;
+import fr.raksrinana.channelpointsminer.miner.api.ws.TwitchPubSubWebSocketClient;
 import fr.raksrinana.channelpointsminer.miner.tests.ParallelizableTest;
 import org.assertj.core.api.Assertions;
+import org.mockito.Mock;
 import org.junit.jupiter.api.Test;
 
 @ParallelizableTest
 class TwitchPubSubWebSocketClientFactoryTest{
+	@Mock
+	private TwitchLogin twitchLogin;
+	
 	@Test
-	void create(){
-		Assertions.assertThat(TwitchWebSocketClientFactory.createPubSubClient()).isNotNull();
+	void createPubSub(){
+		Assertions.assertThat(TwitchWebSocketClientFactory.createPubSubClient()).isNotNull().isInstanceOf(TwitchPubSubWebSocketClient.class);
+	}
+	
+	@Test
+	void createIrc(){
+		Assertions.assertThat(TwitchWebSocketClientFactory.createIrcClient(twitchLogin)).isNotNull().isInstanceOf(TwitchChatWebSocketClient.class);
 	}
 }
