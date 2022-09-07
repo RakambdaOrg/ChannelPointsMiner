5091e740e7b5a2395111ca5d7b356604154b7ae3
Thomas Couchoud <1688389+RakSrinaNa@users.noreply.github.com>
2022-09-07 21:29:51 +0200

    Updates on moments claiming

--- miner/src/main/java/fr/raksrinana/channelpointsminer/miner/api/ws/data/message/communitymoment/CommunityMomentStartData.java
+++ miner/src/main/java/fr/raksrinana/channelpointsminer/miner/api/ws/data/message/communitymoment/CommunityMomentStartData.java
@@ -19,4 +19,10 @@ public class CommunityMomentStartData{
 	@JsonProperty("moment_id")
 	@NotNull
 	private String momentId;
+	@JsonProperty("channel_id")
+	@NotNull
+	private String channelId;
+	@JsonProperty("clip_slug")
+	@NotNull
+	private String clipSlug;
 }
