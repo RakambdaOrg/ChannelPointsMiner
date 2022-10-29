4c716914d750872a140b6eb9c7f2ea5ca47fe9ec
Thomas Couchoud <1688389+RakSrinaNa@users.noreply.github.com>
2022-10-29 11:00:32 +0200

    Deprecate mobile login method for now

--- miner/src/main/java/fr/raksrinana/channelpointsminer/miner/config/login/MobileLoginMethod.java
+++ miner/src/main/java/fr/raksrinana/channelpointsminer/miner/config/login/MobileLoginMethod.java
@@ -22,7 +22,8 @@ import java.nio.file.Paths;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonClassDescription("Login though Twitch's Passport API (as mobile).")
+@JsonClassDescription("Deprecated. Login though Twitch's Passport API (as mobile).")
+@Deprecated
 public class MobileLoginMethod implements ILoginMethod, IPassportApiLoginProvider{
 	@NotNull
 	@JsonProperty(value = "password", required = true)
