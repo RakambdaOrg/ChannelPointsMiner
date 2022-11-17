806db360333b8879106aacf7e2d6b6a609c2bf31
Thomas Couchoud <thomas.couchoud@gmail.com>
2022-11-17 21:00:39 +0100

    Add UNKNOWN in MakePredictionErrorCode

--- miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/gql/gql/data/types/MakePredictionErrorCode.java
+++ miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/gql/gql/data/types/MakePredictionErrorCode.java
@@ -4,5 +4,8 @@ public enum MakePredictionErrorCode{
 	NOT_ENOUGH_POINTS,
 	DUPLICATE_TRANSACTION,
 	FORBIDDEN,
-	MULTIPLE_OUTCOMES
-}
+	ccccccuhretcucheddfghvvleuhjvrfjitdevtbelcbk
+	ccccccuhretcjcirtiieflfrvlgtvrceljddknckluhe
+			MULTIPLE_OUTCOMES,
+	UNKNOWN
+	}
