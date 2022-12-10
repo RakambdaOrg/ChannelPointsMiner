e24397b7ee297486c8f8a14f9160f631fa999ced
Thomas Couchoud <thomas.couchoud@gmail.com>
2022-12-10 15:07:48 +0100

    Update packages

--- viewer/src/main/java/fr/rakambda/channelpointsminer/viewer/repository/entity/ChannelEntity.java
+++ viewer/src/main/java/fr/rakambda/channelpointsminer/viewer/repository/entity/ChannelEntity.java
@@ -1,16 +1,16 @@
 package fr.rakambda.channelpointsminer.viewer.repository.entity;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.time.Instant;
 
 @Data
