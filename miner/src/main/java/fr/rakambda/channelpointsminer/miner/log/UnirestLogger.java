35ee1fbf3bffcc1f4ad7791876287ee2414acbc9
Thomas Couchoud <thomas.couchoud@gmail.com>
2022-12-24 17:19:41 +0100

    Stop logging token 400 error all the time

--- miner/src/main/java/fr/rakambda/channelpointsminer/miner/log/UnirestLogger.java
+++ miner/src/main/java/fr/rakambda/channelpointsminer/miner/log/UnirestLogger.java
@@ -6,9 +6,12 @@ import kong.unirest.core.HttpRequestSummary;
 import kong.unirest.core.HttpResponse;
 import kong.unirest.core.Interceptor;
 import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
 
 @Log4j2
 public class UnirestLogger implements Interceptor{
+	private static final String TOKEN_URL = "https://id.twitch.tv/oauth2/token";
+	
 	@Override
 	public void onRequest(HttpRequest<?> request, Config config){
 		log.trace("Sending request to {}", request.getUrl());
@@ -26,9 +29,21 @@ public class UnirestLogger implements Interceptor{
 			return;
 		}
 		
-		if(!(response.getParsingError().isPresent() && response.getStatus() == 204)){
+		if(shouldLogError(request, response)){
 			log.error("Failed to request {} got statusCode `{}` and parsing error: {}", request.getUrl(), response.getStatus(), response.getParsingError());
 			response.getParsingError().ifPresent(ex -> log.error("Failed to parse body: {}", ex.getOriginalBody()));
 		}
+	}
+	
+	private static boolean shouldLogError(@NotNull HttpRequestSummary request, @NotNull HttpResponse<?> response){
+		if(response.getParsingError().isPresent() && response.getStatus() == 204){
+			return false;
+		}
+		
+		if(request.getUrl().equals(TOKEN_URL) && response.getStatus() == 400){
+			return false;
+		}
+		
+		return true;
 	}
 }
