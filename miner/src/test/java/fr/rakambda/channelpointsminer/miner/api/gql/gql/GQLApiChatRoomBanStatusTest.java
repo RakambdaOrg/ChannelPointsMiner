77ea2fb8fa22e368099975b4455565b7de903f0a
Thomas Couchoud <thomas.couchoud@gmail.com>
2023-02-15 23:05:13 +0100

    feat: parse moderator value properly

--- miner/src/test/java/fr/rakambda/channelpointsminer/miner/api/gql/gql/GQLApiChatRoomBanStatusTest.java
+++ miner/src/test/java/fr/rakambda/channelpointsminer/miner/api/gql/gql/GQLApiChatRoomBanStatusTest.java
@@ -16,68 +16,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(UnirestMockExtension.class)
 class GQLApiChatRoomBanStatusTest extends AbstractGQLTest{
-	private static final String USERNAME = "username";
-	private static final String CHANNEL = "channel";
-	
-	@Test
-	void notBanned(){
-		var expected = GQLResponse.<ChatRoomBanStatusData> builder()
-				.extensions(Map.of(
-						"durationMilliseconds", 21,
-						"operationName", "ChatRoomBanStatus",
-						"requestID", "request-id"
-				))
-				.data(ChatRoomBanStatusData.builder()
-						.targetUser(User.builder()
-								.id("123456789")
-								.login("target-username")
-								.build())
-						.build())
-				.build();
-		
-		expectValidRequestOkWithIntegrityOk("api/gql/gql/channelRoomBanStatus_notBanned.json");
-		
-		assertThat(tested.chatRoomBanStatus(CHANNEL, USERNAME)).isPresent().get().isEqualTo(expected);
-		
-		verifyAll();
-	}
-	
-	@Test
-	void banned(){
-		var expected = GQLResponse.<ChatRoomBanStatusData> builder()
-				.extensions(Map.of(
-						"durationMilliseconds", 21,
-						"operationName", "ChatRoomBanStatus",
-						"requestID", "request-id"
-				))
-				.data(ChatRoomBanStatusData.builder()
-						.chatRoomBanStatus(ChatRoomBanStatus.builder()
-								.bannedUser(User.builder()
-										.id("123456789")
-										.login("target-username")
-										.displayName("target-name")
-										.build())
-								.createdAt(ZonedDateTime.of(2022, 1, 1, 10, 22, 35, 0, UTC))
-								.permanent(true)
-								.reason("")
-								.build())
-						.targetUser(User.builder()
-								.id("123456789")
-								.login("target-username")
-								.build())
-						.build())
-				.build();
-		
-		expectValidRequestOkWithIntegrityOk("api/gql/gql/channelRoomBanStatus_banned.json");
-		
-		var actual = tested.chatRoomBanStatus(CHANNEL, USERNAME);
-		assertThat(actual).isPresent().get().isEqualTo(expected);
-		
-		verifyAll();
-	}
-	
-	@Override
-	protected String getValidRequest(){
-		return "{\"extensions\":{\"persistedQuery\":{\"sha256Hash\":\"319f2a9a3ac7ddecd7925944416c14b818b65676ab69da604460b68938d22bea\",\"version\":1}},\"operationName\":\"ChatRoomBanStatus\",\"variables\":{\"targetUserID\":\"%s\",\"channelID\":\"%s\"}}".formatted(USERNAME, CHANNEL);
-	}
+    private static final String USERNAME = "username";
+    private static final String CHANNEL = "channel";
+    
+    @Test
+    void notBanned(){
+        var expected = GQLResponse.<ChatRoomBanStatusData> builder()
+                .extensions(Map.of(
+                        "durationMilliseconds", 21,
+                        "operationName", "ChatRoomBanStatus",
+                        "requestID", "request-id"
+                ))
+                .data(ChatRoomBanStatusData.builder()
+                        .targetUser(User.builder()
+                                .id("123456789")
+                                .login("target-username")
+                                .build())
+                        .build())
+                .build();
+        
+        expectValidRequestOkWithIntegrityOk("api/gql/gql/channelRoomBanStatus_notBanned.json");
+        
+        assertThat(tested.chatRoomBanStatus(CHANNEL, USERNAME)).isPresent().get().isEqualTo(expected);
+        
+        verifyAll();
+    }
+    
+    @Test
+    void banned(){
+        var expected = GQLResponse.<ChatRoomBanStatusData> builder()
+                .extensions(Map.of(
+                        "durationMilliseconds", 21,
+                        "operationName", "ChatRoomBanStatus",
+                        "requestID", "request-id"
+                ))
+                .data(ChatRoomBanStatusData.builder()
+                        .chatRoomBanStatus(ChatRoomBanStatus.builder()
+                                .bannedUser(User.builder()
+                                        .id("123456789")
+                                        .login("target-username")
+                                        .displayName("target-name")
+                                        .build())
+                                .createdAt(ZonedDateTime.of(2022, 1, 1, 10, 22, 35, 0, UTC))
+                                .permanent(true)
+                                .reason("a reason")
+                                .moderator(User.builder()
+                                        .id("741852963")
+                                        .login("modo")
+                                        .displayName("ModoName")
+                                        .build())
+                                .build())
+                        .targetUser(User.builder()
+                                .id("123456789")
+                                .login("target-username")
+                                .build())
+                        .build())
+                .build();
+        
+        expectValidRequestOkWithIntegrityOk("api/gql/gql/channelRoomBanStatus_banned.json");
+        
+        var actual = tested.chatRoomBanStatus(CHANNEL, USERNAME);
+        assertThat(actual).isPresent().get().isEqualTo(expected);
+        
+        verifyAll();
+    }
+    
+    @Override
+    protected String getValidRequest(){
+        return "{\"extensions\":{\"persistedQuery\":{\"sha256Hash\":\"319f2a9a3ac7ddecd7925944416c14b818b65676ab69da604460b68938d22bea\",\"version\":1}},\"operationName\":\"ChatRoomBanStatus\",\"variables\":{\"targetUserID\":\"%s\",\"channelID\":\"%s\"}}".formatted(USERNAME, CHANNEL);
+    }
 }
