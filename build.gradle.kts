86da81f62624ecd0bdcb09a8c1e098938c7291ba
Thomas Couchoud <thomas.couchoud@gmail.com>
2023-02-15 23:33:06 +0100

    ci: accept scan TOS

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,10 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "idea")
 }
+
+if (hasProperty("buildScan")) {
+    extensions.findByName("buildScan")?.withGroovyBuilder {
+        setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+        setProperty("termsOfServiceAgree", "yes")
+    }
+}
