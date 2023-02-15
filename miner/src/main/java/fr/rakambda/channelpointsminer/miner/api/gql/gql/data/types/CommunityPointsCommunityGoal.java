7e97bfbf31213b7b0a65078f07ea08fb88aa6d96
Thomas Couchoud <thomas.couchoud@gmail.com>
2023-02-15 23:56:13 +0100

    fix(GH-423): add type field to goal

--- miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/gql/gql/data/types/CommunityPointsCommunityGoal.java
+++ miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/gql/gql/data/types/CommunityPointsCommunityGoal.java
@@ -11,7 +11,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import java.awt.Color;
+import java.awt.*;
 import java.time.ZonedDateTime;
 
 @JsonTypeName("CommunityPointsCommunityGoal")
@@ -22,37 +22,39 @@ import java.time.ZonedDateTime;
 @EqualsAndHashCode(callSuper = true)
 @ToString
 public class CommunityPointsCommunityGoal extends GQLType{
-	@JsonProperty("backgroundColor")
-	@JsonDeserialize(using = ColorDeserializer.class)
-	private Color backgroundColor;
-	@JsonProperty("description")
-	private String description;
-	@JsonProperty("durationDays")
-	private int durationDays;
-	@JsonProperty("endedAt")
-	@JsonDeserialize(using = ISO8601ZonedDateTimeDeserializer.class)
-	private ZonedDateTime endedAt;
-	@JsonProperty("amountNeeded")
-	private int amountNeeded;
-	@JsonProperty("id")
-	private String id;
-	@JsonProperty("defaultImage")
-	private CommunityPointsImage defaultImage;
-	@JsonProperty("image")
-	private CommunityPointsImage image;
-	@JsonProperty("isInStock")
-	private boolean inStock;
-	@JsonProperty("smallContribution")
-	private int smallContribution;
-	@JsonProperty("perStreamUserMaximumContribution")
-	private int perStreamUserMaximumContribution;
-	@JsonProperty("pointsContributed")
-	private int pointsContributed;
-	@JsonProperty("startedAt")
-	@JsonDeserialize(using = ISO8601ZonedDateTimeDeserializer.class)
-	private ZonedDateTime startedAt;
-	@JsonProperty("status")
-	private CommunityPointsCommunityGoalStatus status;
-	@JsonProperty("title")
-	private String title;
+    @JsonProperty("backgroundColor")
+    @JsonDeserialize(using = ColorDeserializer.class)
+    private Color backgroundColor;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("durationDays")
+    private int durationDays;
+    @JsonProperty("endedAt")
+    @JsonDeserialize(using = ISO8601ZonedDateTimeDeserializer.class)
+    private ZonedDateTime endedAt;
+    @JsonProperty("amountNeeded")
+    private int amountNeeded;
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("defaultImage")
+    private CommunityPointsImage defaultImage;
+    @JsonProperty("image")
+    private CommunityPointsImage image;
+    @JsonProperty("isInStock")
+    private boolean inStock;
+    @JsonProperty("smallContribution")
+    private int smallContribution;
+    @JsonProperty("perStreamUserMaximumContribution")
+    private int perStreamUserMaximumContribution;
+    @JsonProperty("pointsContributed")
+    private int pointsContributed;
+    @JsonProperty("startedAt")
+    @JsonDeserialize(using = ISO8601ZonedDateTimeDeserializer.class)
+    private ZonedDateTime startedAt;
+    @JsonProperty("status")
+    private CommunityPointsCommunityGoalStatus status;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("type")
+    private String type;
 }
