72ce1cb3b0aa6c3d42509d855e69c0525a6d9f3e
Thomas Couchoud <thomas.couchoud@gmail.com>
2023-02-15 23:03:53 +0100

    fix: UnirestMockExtension support several tests at once

--- miner/src/test/java/fr/rakambda/channelpointsminer/miner/tests/UnirestMockExtension.java
+++ miner/src/test/java/fr/rakambda/channelpointsminer/miner/tests/UnirestMockExtension.java
@@ -2,7 +2,6 @@ package fr.rakambda.channelpointsminer.miner.tests;
 
 import fr.rakambda.channelpointsminer.miner.util.json.JacksonUtils;
 import kong.unirest.core.Config;
-import kong.unirest.core.HttpRequest;
 import kong.unirest.core.HttpRequestSummary;
 import kong.unirest.core.HttpResponse;
 import kong.unirest.core.Interceptor;
@@ -18,46 +17,49 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 @Log4j2
 public class UnirestMockExtension implements Extension, BeforeEachCallback, AfterEachCallback, ParameterResolver{
-	private UnirestInstance unirestInstance;
-	private UnirestMock unirestMock;
-	
-	@Override
-	public void beforeEach(ExtensionContext context){
-		unirestInstance = Unirest.spawnInstance();
-		unirestInstance.config().reset()
-				.clearDefaultHeaders()
-				.setObjectMapper(new JacksonObjectMapper(JacksonUtils.getMapper()))
-				.interceptor(new Interceptor(){
-					@Override
-					public void onRequest(HttpRequest<?> request, Config config){
-					}
-					
-					@Override
-					public void onResponse(HttpResponse<?> response, HttpRequestSummary request, Config config){
-						if(!response.isSuccess()){
-							response.getParsingError().ifPresent(ex -> log.error("Failed to parse body: {}", ex.getOriginalBody(), ex));
-						}
-					}
-				});
-		unirestMock = new UnirestMock(unirestInstance);
-	}
-	
-	@Override
-	public void afterEach(ExtensionContext context){
-		MockClient.clear(unirestInstance);
-	}
-	
-	@Override
-	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException{
-		return Objects.equals(parameterContext.getParameter().getType(), UnirestMock.class);
-	}
-	
-	@Override
-	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException{
-		return unirestMock;
-	}
+    private final Map<String, UnirestInstance> unirestInstance = new HashMap<>();
+    private final Map<String, UnirestMock> unirestMock = new HashMap<>();
+    
+    @Override
+    public void beforeEach(ExtensionContext context){
+        var instance = Unirest.spawnInstance();
+        instance.config().reset()
+                .clearDefaultHeaders()
+                .setObjectMapper(new JacksonObjectMapper(JacksonUtils.getMapper()))
+                .interceptor(new Interceptor(){
+                    @Override
+                    public void onResponse(HttpResponse<?> response, HttpRequestSummary request, Config config){
+                        if(!response.isSuccess()){
+                            response.getParsingError().ifPresent(ex -> log.error("Failed to parse body: {}", ex.getOriginalBody(), ex));
+                        }
+                    }
+                });
+        
+        unirestInstance.put(context.getUniqueId(), instance);
+        unirestMock.put(context.getUniqueId(), new UnirestMock(instance));
+    }
+    
+    @Override
+    public void afterEach(ExtensionContext context){
+        Optional.ofNullable(unirestInstance.get(context.getUniqueId())).ifPresent(MockClient::clear);
+        unirestInstance.remove(context.getUniqueId());
+        unirestMock.remove(context.getUniqueId());
+    }
+    
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException{
+        return Objects.equals(parameterContext.getParameter().getType(), UnirestMock.class);
+    }
+    
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException{
+        return unirestMock.get(extensionContext.getUniqueId());
+    }
 }
