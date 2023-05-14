RakambdaOrg/ChannelPointsMiner

commit 89d016052363bd507962ae7cbc35302b114a98b2

chore(deps): bump unirest-version from 4.0.0-RC7 to 4.0.0-RC9 (#501)
* chore(deps): bump unirest-version from 4.0.0-RC7 to 4.0.0-RC9

Bumps `unirest-version` from 4.0.0-RC7 to 4.0.0-RC9.

Updates `com.konghq:unirest-java-core` from 4.0.0-RC7 to 4.0.0-RC9

Updates `com.konghq:unirest-objectmapper-jackson` from 4.0.0-RC7 to 4.0.0-RC9

Updates `com.konghq:unirest-mocks` from 4.0.0-RC7 to 4.0.0-RC9
- [Release notes](https://github.com/Kong/unirest-java/releases)
- [Changelog](https://github.com/Kong/unirest-java/blob/main/CHANGELOG.md)
- [Commits](https://github.com/Kong/unirest-java/compare/v4.0.0-RC7...v4.0.0-RC9)

---
updated-dependencies:
- dependency-name: com.konghq:unirest-java-core
  dependency-type: direct:production
  update-type: version-update:semver-patch
- dependency-name: com.konghq:unirest-objectmapper-jackson
  dependency-type: direct:production
  update-type: version-update:semver-patch
- dependency-name: com.konghq:unirest-mocks
  dependency-type: direct:production
  update-type: version-update:semver-patch
...

Signed-off-by: dependabot[bot] <support@github.com>

* Use Jackson BOM

---------

Signed-off-by: dependabot[bot] <support@github.com>
Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
Co-authored-by: Thomas Couchoud <thomas.couchoud@gmail.com>

miner/src/main/java/fr/rakambda/channelpointsminer/miner/util/json/URLDeserializer.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.regex.Pattern;
 import static java.util.Objects.nonNull;
@@ -33,7 +34,7 @@ public class URLDeserializer extends StdDeserializer<URL>{
 				if(!SCHEME_PATTERN.matcher(value).matches()){
 					value = "https://" + value;
 				}
-				return new URL(value);
+				return URI.create(value).toURL();
 			}
 		}
 		catch(MalformedURLException e){

gradle/libs.versions.toml
@@ -2,7 +2,7 @@
 slf4j-version = "2.0.7"
 log4j2-version = "2.20.0"
 log4j2-slf4j-version = "2.20.0"
-unirest-version = "4.0.0-RC7"
+unirest-version = "4.0.0-RC9"
 picocli-version = "4.7.3"
 jackson-version = "2.15.0"
 jsonschema-generator-version = "4.31.1"
@@ -43,9 +43,11 @@ unirest-java = { group = "com.konghq", name = "unirest-java-core", version.ref =
 unirest-objectmapper-jackson = { group = "com.konghq", name = "unirest-objectmapper-jackson", version.ref = "unirest-version" }
 unirestMocks = { group = "com.konghq", name = "unirest-mocks", version.ref = "unirest-version" }
 picocli = { group = "info.picocli", name = "picocli", version.ref = "picocli-version" }
-jackson-core = { group = "com.fasterxml.jackson.core", name = "jackson-core", version.ref = "jackson-version" }
-jackson-annotations = { group = "com.fasterxml.jackson.core", name = "jackson-annotations", version.ref = "jackson-version" }
-jackson-databind = { group = "com.fasterxml.jackson.core", name = "jackson-databind", version.ref = "jackson-version" }
+jacksonBom = { group = "com.fasterxml.jackson", name = "jackson-bom", version.ref = "jackson-version"}
+jackson-core = { group = "com.fasterxml.jackson.core", name = "jackson-core" }
+jackson-annotations = { group = "com.fasterxml.jackson.core", name = "jackson-annotations" }
+jackson-databind = { group = "com.fasterxml.jackson.core", name = "jackson-databind" }
+jackson-jsr310 = { group = "com.fasterxml.jackson.datatype", name = "jackson-datatype-jsr310" }
 jsonschema-generator = { group = "com.github.victools", name = "jsonschema-generator", version.ref = "jsonschema-generator-version" }
 jsonschema-module-jackson = { group = "com.github.victools", name = "jsonschema-module-jackson", version.ref = "jsonschema-generator-version" }
 httpclient = { group = "org.apache.httpcomponents", name = "httpclient", version.ref = "httpclient-version" }
@@ -77,7 +79,7 @@ json-unit-assertj = { group = "net.javacrumbs.json-unit", name = "json-unit-asse
 rerunnerJupiter = { group = "io.github.artsok", name = "rerunner-jupiter", version.ref = "rerunner-jupiter-version" }
 
 [bundles]
-jackson = ["jackson-core", "jackson-annotations", "jackson-databind"]
+jackson = ["jackson-core", "jackson-annotations", "jackson-databind", "jackson-jsr310"]
 log4j2 = ["log4j2-core", "log4j2-slf4j"]
 unirest = ["unirest-java", "unirest-objectmapper-jackson"]
 flyway = ["flyway-core", "flyway-mysql"]

miner/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
 }
 
 dependencies {
+    implementation(platform(libs.jacksonBom))
+
     implementation(libs.slf4j)
     implementation(libs.bundles.log4j2)
 

miner/src/main/java/fr/rakambda/channelpointsminer/miner/browser/Browser.java
@@ -26,6 +26,7 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Collection;
@@ -139,13 +140,13 @@ public class Browser implements AutoCloseable{
 	@SneakyThrows
 	@NotNull
 	private RemoteWebDriver getRemoteDriverChrome(@NotNull BrowserConfiguration configuration){
-		return new RemoteWebDriver(new URL(configuration.getRemoteHost()), getDefaultChromeOptions(configuration));
+		return new RemoteWebDriver(URI.create(configuration.getRemoteHost()).toURL(), getDefaultChromeOptions(configuration));
 	}
 	
 	@SneakyThrows
 	@NotNull
 	private RemoteWebDriver getRemoteDriverFirefox(@NotNull BrowserConfiguration configuration){
-		return new RemoteWebDriver(new URL(configuration.getRemoteHost()), getDefaultFirefoxOptions(configuration));
+		return new RemoteWebDriver(URI.create(configuration.getRemoteHost()).toURL(), getDefaultFirefoxOptions(configuration));
 	}
 	
 	@NotNull

commit a294cf49b1abff9de4a5ce4fa0dd2576b3f16bd4

Fix new URI error introduced with 89d016052363bd507962ae7cbc35302b114a98b2, should be a warn message

miner/src/main/java/fr/rakambda/channelpointsminer/miner/util/json/URLDeserializer.java
@@ -37,7 +37,7 @@ public class URLDeserializer extends StdDeserializer<URL>{
 				return URI.create(value).toURL();
 			}
 		}
-		catch(MalformedURLException e){
+		catch(MalformedURLException | IllegalArgumentException e){
 			log.warn("Failed to parse URL: {}", jsonParser.getValueAsString(), e);
 		}
 		return null;