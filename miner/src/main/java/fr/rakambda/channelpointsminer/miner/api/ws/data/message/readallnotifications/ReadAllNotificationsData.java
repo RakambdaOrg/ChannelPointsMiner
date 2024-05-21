044c701820eddb0c3cef9689bd78e6a48c852e29
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2024-05-21 12:28:21 +0100

    Attempt to add new/missing websocket message

---
+++ miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/ws/data/message/readallnotifications/ReadAllNotificationsData.java
@@ -0,0 +1,31 @@
+package fr.rakambda.channelpointsminer.miner.api.ws.data.message.readallnotifications;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import fr.rakambda.channelpointsminer.miner.api.ws.data.message.subtype.NotificationSummary;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.jetbrains.annotations.NotNull;
+import java.util.LinkedList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+@Builder
+public class ReadAllNotificationsData{
+	@JsonProperty("notification_ids")
+	@NotNull
+	private List<String> notificationIds = new LinkedList<>();
+	@JsonProperty("display_type")
+	@NotNull
+	private String displayType;
+	@JsonProperty("summary")
+	@NotNull
+	private NotificationSummary summary;
+}
