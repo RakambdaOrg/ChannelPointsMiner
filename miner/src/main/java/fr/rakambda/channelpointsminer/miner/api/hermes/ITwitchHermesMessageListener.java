502f2e0f760778353bc1e00342d69ac2c711723d
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-08-23 14:53:13 +0200

    Remove unused method

--- miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/hermes/ITwitchHermesMessageListener.java
+++ miner/src/main/java/fr/rakambda/channelpointsminer/miner/api/hermes/ITwitchHermesMessageListener.java
@@ -1,8 +1,4 @@
 package fr.rakambda.channelpointsminer.miner.api.hermes;
 
-import fr.rakambda.channelpointsminer.miner.api.pubsub.data.message.IPubSubMessage;
-import org.jetbrains.annotations.NotNull;
-
 public interface ITwitchHermesMessageListener{
-	void onPubSubNotification(@NotNull IPubSubMessage message);
 }
