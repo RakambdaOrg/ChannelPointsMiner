6ace54260ff6fed29056dc73c9d1ae4f91d9a6d3
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-11-21 09:16:37 +0100

    Migration

--- viewer/src/test/java/fr/rakambda/channelpointsminer/viewer/api/ApiControllerTest.java
+++ viewer/src/test/java/fr/rakambda/channelpointsminer/viewer/api/ApiControllerTest.java
@@ -3,7 +3,8 @@ package fr.rakambda.channelpointsminer.viewer.api;
 import fr.rakambda.channelpointsminer.viewer.api.data.BalanceData;
 import fr.rakambda.channelpointsminer.viewer.api.data.ChannelData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
 @WebMvcTest(ApiController.class)
 class ApiControllerTest{
     @MockitoBean
